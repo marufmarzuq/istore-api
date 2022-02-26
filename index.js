bab8efb028dc6a2f7a07d7727f124e6744e6da98
marufmarzuq <marufmarzuq18@gmail.com>
2022-02-26 11:39:56 +0600

    Api models completed

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("Connceted database successfull!"))
+    .catch((err) => console.log(err));
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
+
+app.listen(process.env.PORT || 5000, () => console.log("backend server is running in port", 5000));

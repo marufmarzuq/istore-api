0d7c3127177cd86388c8aebef3bb689f211aaef8
marufmarzuq <marufmarzuq18@gmail.com>
2022-06-27 00:19:29 +0600

    Models completed

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ const authRoute = require("./routes/auth");
 dotenv.config();
 
 mongoose
-    .connect(process.env.MONGO_URL)
-    .then(() => console.log("Connceted database successfull!"))
-    .catch((err) => console.log(err));
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Connceted database successfull!"))
+  .catch((err) => console.log(err));
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 
-app.listen(process.env.PORT || 5000, () => console.log("backend server is running in port", 5000));
+app.listen(process.env.PORT || 5000, () =>
+  console.log("backend server is running in port", 5000)
+);
--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,8 @@
 const router = require("express").Router();
 
+router.post("/userpost", (req, res) => {
+  console.log(req.body);
+  res.send(req.body.uid);
+});
+
 module.exports = router;

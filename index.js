6faaf87016451de2dda4b2b7c58b09df182f0c4c
marufmarzuq <marufmarzuq18@gmail.com>
2022-06-27 12:18:12 +0600

    cart route added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
 const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/cart", cartRoute);
 
 app.listen(process.env.PORT || 5000, () =>
   console.log("backend server is running in port", 5000)

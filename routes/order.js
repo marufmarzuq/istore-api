3476244488bab76e80fca4e7db39c0a6c3d5e7c2
marufmarzuq <marufmarzuq18@gmail.com>
2022-06-27 12:39:03 +0600

    Order route get order

--- routes/order.js
+++ routes/order.js
@@ -48,18 +48,69 @@ router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
 // Get user orders
 router.get("/find/:userId", verifyTokenAndAuth, async (req, res) => {
   try {
-    const cart = await Cart.findOne({ userId: req.params.userId });
-    res.status(200).json(cart);
+    const orders = await Order.find({ userId: req.params.userId });
+    res.status(200).json(orders);
   } catch (error) {
     res.status(500).json(error);
   }
 });
 
-// Get all carts
+// Get all orders
 router.get("/", verifyTokenAndAdmin, async (req, res) => {
   try {
-    const carts = await Cart.find();
+    const orders = await Order.find();
     res.status(200).json(carts);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Get orders stats
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+  try {
+    const data = await User.aggregate([
+      { $match: { createdAt: { $gte: lastYear } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Get monthly income
+router.get("/income", verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+  const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
+  try {
+    const data = await User.aggregate([
+      { $match: { createdAt: { $gte: lastYear } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data);
   } catch (error) {
     res.status(500).json(error);
   }

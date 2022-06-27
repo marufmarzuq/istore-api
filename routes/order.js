c96bc3ec90cf0b2ede7128265b7e0b43e2c1ab25
marufmarzuq <marufmarzuq18@gmail.com>
2022-06-27 12:42:59 +0600

    Income stats added

--- routes/order.js
+++ routes/order.js
@@ -65,52 +65,28 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
-// Get orders stats
-router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
-  const date = new Date();
-  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
-  try {
-    const data = await User.aggregate([
-      { $match: { createdAt: { $gte: lastYear } } },
-      {
-        $project: {
-          month: { $month: "$createdAt" },
-        },
-      },
-      {
-        $group: {
-          _id: "$month",
-          total: { $sum: 1 },
-        },
-      },
-    ]);
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
-
 // Get monthly income
 router.get("/income", verifyTokenAndAdmin, async (req, res) => {
   const date = new Date();
   const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
   const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
   try {
-    const data = await User.aggregate([
-      { $match: { createdAt: { $gte: lastYear } } },
+    const income = await Order.aggregate([
+      { $match: { createdAt: { $gte: previousMonth } } },
       {
         $project: {
           month: { $month: "$createdAt" },
+          sales: "$amount",
         },
       },
       {
         $group: {
           _id: "$month",
-          total: { $sum: 1 },
+          total: { $sum: "$sales" },
         },
       },
     ]);
-    res.status(200).json(data);
+    res.status(200).json(income);
   } catch (error) {
     res.status(500).json(error);
   }

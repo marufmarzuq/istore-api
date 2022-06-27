0508e172138dd0dfc5ed044a02f1b7805a3ce5cc
marufmarzuq <marufmarzuq18@gmail.com>
2022-06-27 12:01:17 +0600

    Product filter added

--- routes/product.js
+++ routes/product.js
@@ -53,8 +53,17 @@ router.get("/find/:id", async (req, res) => {
 
 // Get all products
 router.get("/", async (req, res) => {
+  const qNew = req.query.new;
+  const qCategory = req.query.category;
   try {
-    const products = await Product.find();
+    let products;
+    if (qNew) {
+      products = await Product.find().sort({ createdAt: -1 }).limit(5);
+    } else if (qCategory) {
+      products = await Product.find({ categories: { $in: [qCategory] } });
+    } else {
+      products = await Product.find();
+    }
     res.status(200).json(products);
   } catch (error) {
     res.status(500).json(error);
